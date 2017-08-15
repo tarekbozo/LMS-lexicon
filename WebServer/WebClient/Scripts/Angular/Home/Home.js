062f131a933ccad767a80a6ff0261340586c1a4e
Gommash <a.lekedal@gmail.com>
2017-08-15 16:05:06 +0200

    some important files

---
+++ WebServer/WebClient/Scripts/Angular/Home/Home.js
@@ -0,0 +1,9 @@
+﻿app.controller("HomeController", ["$scope", "$http", function ($scope, $http) {
+
+    $scope.getData = function () {
+        $http.get('http://localhost:52255/api/HomeAPI/Get/')
+            .then(function (response) {
+                $scope.data = response.data;
+            })
+    };
+}]);
