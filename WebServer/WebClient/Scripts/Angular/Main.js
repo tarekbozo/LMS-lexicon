945cb761f3d9f649b151d0a15fd5db477d24122f
Gommash <a.lekedal@gmail.com>
2017-08-15 14:44:03 +0200

    project

---
+++ WebServer/WebClient/Scripts/Angular/Main.js
@@ -0,0 +1,12 @@
+﻿(function () {
+    var app = angular.module("LMSApp", []);
+    app.controller("MainController", ["$scope", "$http", function ($scope, $http) {
+
+        $scope.getData = function () {
+            $http.get('http://localhost:52255/api/HomeAPI/Get/')
+                .then(function (response) {
+                    $scope.data = response.data;
+                })
+        };
+    }]);
+}());
---
+++ WebServer/WebServer/App_Start/WebApiConfig.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web.Http;
+using Microsoft.Owin.Security.OAuth;
+using Newtonsoft.Json.Serialization;
+using System.Net.Http.Headers;
+
+namespace WebServer
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            config.EnableCors();
+
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{action}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+
+            config.Formatters
+                  .JsonFormatter
+                  .SupportedMediaTypes
+                  .Add(new MediaTypeHeaderValue("text/html"));
+
+            config.Formatters
+                  .JsonFormatter
+                  .SerializerSettings
+                  .ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
+        }
+    }
+}
