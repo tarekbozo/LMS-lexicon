3e89da97e1dfaf9f58a20c129bd6f1837af646ae
Gommash <a.lekedal@gmail.com>
2017-08-17 10:27:41 +0200

    forgot to change an important thing

--- LMS-Lexicon/WebClient_Lexicon/Scripts/Angular/Home.js
+++ LMS-Lexicon/WebClient_Lexicon/Scripts/Angular/Home.js
@@ -3,7 +3,7 @@ var app = angular.module("LMSApp");
     app.controller("HomeController", ["$scope", "$http", function ($scope, $http) {
 
         $scope.getData = function () {
-            $http.get('/api/HomeAPI/Get/')
+            $http.get('http://localhost:51942/api/HomeAPI/Get/')
                 .then(function (response) {
                     $scope.data = response.data;
                 })
