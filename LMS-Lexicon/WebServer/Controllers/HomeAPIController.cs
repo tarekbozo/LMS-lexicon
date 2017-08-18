65a4bdf3015ce07b7a4ab24c938fef4d750c9e3f
AhAh10 <ahmadi2001@yahoo.com>
2017-08-18 15:02:25 +0200

    Commiting stuff

--- LMS-Lexicon/WebServer/Controllers/HomeAPIController.cs
+++ LMS-Lexicon/WebServer/Controllers/HomeAPIController.cs
@@ -18,6 +18,7 @@ namespace WebServer.Controllers
             return new string[] { "value1", "value2" };
         }
 
+
         // GET api/values/5
         public string Get(int id)
         {
---
+++ LMS-Lexicon/WebServer/Models/LMS/Attendance.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace WebServer.Models.LMS
+{
+    public class Attendance
+    {
+        [Key]
+        public int ID { get; set; }
+
+        [Display(Name = "Week day")]
+        public WeekDays WeekDay { get; set; }
+
+        [Display(Name = "First name")]
+        public string FirstName { get; set; }
+
+        [Display(Name = "Last name")]
+        public string LastName { get; set; }
+
+        [Display(Name = "Birth Date")]
+        public string BirthDate { get; set; }
+
+        [Required]
+        [ForeignKey("Student")]
+        public int UserID { get; set; }
+        public virtual User Student { get; set; }
+    }
+}
