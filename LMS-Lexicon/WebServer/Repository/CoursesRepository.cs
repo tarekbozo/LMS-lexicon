413a050889968a4b22c636bf2563b1a5768a7dd6
Gommash <a.lekedal@gmail.com>
2017-08-23 16:27:52 +0200

    bootstrap added

---
+++ LMS-Lexicon/WebServer/Repository/CoursesRepository.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using WebServer.Models;
+using WebServer.Models.LMS;
+
+namespace WebServer.Repository
+{
+    public class CoursesRepository:IDisposable
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        public IEnumerable<Course> Courses()
+        {
+            return db.Courses;
+        }
+
+        public Course Course(int? id)
+        {
+            return Courses().SingleOrDefault(c => c.ID == id);
+        }
+
+        public IEnumerable<Course> StudentCourses(string studentId)
+        {
+            return db.LMSUsers.FirstOrDefault(u => u.Id == studentId)
+                              .Schedules
+                              .Select(s => s.Course)
+                              .OrderBy(c => c.Subject.Name)
+                              .ThenBy(c => c.Teacher.ToString());
+        }
+
+        public IEnumerable<Course> TeacherCourse(string teacherId)
+        {
+            return db.LMSUsers.FirstOrDefault(u => u.Id == teacherId)
+                              .Courses
+                              .OrderBy(c => c.Subject.Name)
+                              .ThenBy(c => c.Teacher.ToString());
+        }
+
+        public bool Add(Course course)
+        {
+            if (course.TeacherID != null)
+            {
+                var _courses = this.Courses().Where(c => c.TeacherID == course.TeacherID && c.SubjectID == course.SubjectID);
+                if (_courses.Count() != 0)
+                {
+                    _courses = null;
+                    return false;
+                }
+                db.Courses.Add(course);
+                SaveChanges();
+                return true;
+            }
+            return false;
+        }
+
+        public bool Edit(Course course)
+        {
+            if (this.db.Courses.Where(_cRel => _cRel.ID != course.ID && _cRel.SubjectID == course.SubjectID && _cRel.TeacherID == course.TeacherID).Count() == 0)
+            {
+                db.Entry(course).State = EntityState.Modified;
+                db.SaveChanges();
+                return true;
+            }
+            return false;
+        }
+
+        public bool Delete(int? id)
+        {
+            Course course = Course(id);
+            if (course != null)
+            {
+                if (course.Documents.Count() == 0 && course.Schedules.Count() == 0)
+                {
+                    db.Subjects.Where(s => s.Courses.Remove(course));
+                    db.Courses.Remove(course);
+                    SaveChanges();
+                    return true;
+                }
+            }
+            return false;
+        }
+        //Save changed
+        private void SaveChanges()
+        {
+            db.SaveChanges();
+        }
+
+        #region IDisposable Support
+        private bool disposedValue = false;
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                db.Dispose();
+                disposedValue = true;
+            }
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+        }
+        #endregion
+    }
+}
---
+++ LMS-Lexicon/WebServer/Repository/SubjectsRepository.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using WebServer.Models;
+using WebServer.Models.LMS;
+
+namespace WebServer.Repository
+{
+    public class SubjectsRepository : IDisposable
+    {
+
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        public IEnumerable<Subject> Subjects()
+        {
+            return db.Subjects;
+        }
+
+        public Subject Subject(int? id)
+        {
+            return Subjects().SingleOrDefault(s => s.ID == id);
+        }
+
+        public Subject Subject(string name)
+        {
+            return Subjects().SingleOrDefault(s => s.Name == name);
+        }
+
+        public bool Add(Subject subject)
+        {
+            if (db.Subjects.FirstOrDefault(s => string.Compare(s.Name, subject.Name, true) == 0) != null)
+            {
+                return false;
+            }
+            db.Subjects.Add(subject);
+            SaveChanges();
+            return true;
+        }
+
+        public bool Edit(Subject subject)
+        {
+            if (db.Subjects.FirstOrDefault(s => string.Compare(s.Name, subject.Name, true) == 0 && s.ID != subject.ID) != null)
+            {
+                return false;
+            }
+            db.Entry(subject).State = EntityState.Modified;
+            db.SaveChanges();
+            return true;
+        }
+
+        public bool Delete(int? id)
+        {
+            Subject subject = Subject(id);
+
+            if (subject != null)
+            {
+                if (subject.Courses.Where(c => c.Documents.Count() == 0 && c.SubjectID == id).Count() == 0)
+                {
+                    db.Subjects.Remove(subject);
+                    SaveChanges();
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public List<Subject> AvaibleSubjects(string user)
+        {
+            List<Subject> Subject_result = new List<Subject>();
+            foreach (Subject s in Subjects().ToList())
+            {
+                if (s.Courses.Count == 0 || s.Courses.Where(c => c.Teacher.UserName == user || c.TeacherID == user).Count() == 0)
+                {
+                    Subject_result.Add(s);
+                }
+            }
+            return Subject_result;
+        }
+
+        private void SaveChanges()
+        {
+            db.SaveChanges();
+        }
+
+        #region IDisposable Support
+        private bool disposedValue = false;
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                db.Dispose();
+                disposedValue = true;
+            }
+        }
+        public void Dispose()
+        {
+            Dispose(true);
+        }
+        #endregion
+    }
+}
