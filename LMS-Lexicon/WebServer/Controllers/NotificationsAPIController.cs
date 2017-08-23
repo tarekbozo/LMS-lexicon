1303a314a4bfec720bc390acd8b8252a56dbc4c3
AhAh10 <ahmadi2001@yahoo.com>
2017-08-23 08:59:25 +0200

    Commiting changes

---
+++ LMS-Lexicon/WebServer/Controllers/NotificationsAPIController.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using WebServer.Models;
+using WebServer.Models.LMS;
+
+namespace WebServer.Controllers
+{
+    public class NotificationsAPIController : ApiController
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: api/NotificationAPIs
+        public IQueryable<Notification> GetNotifications()
+        {
+            return db.Notifications;
+        }
+
+        // GET: api/NotificationAPIs/5
+        [ResponseType(typeof(Notification))]
+        public IHttpActionResult GetNotification(int id)
+        {
+            Notification notification = db.Notifications.Find(id);
+            if (notification == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(notification);
+        }
+
+        // PUT: api/NotificationAPIs/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutNotification(int id, Notification notification)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != notification.ID)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(notification).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!NotificationExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/NotificationAPIs
+        [ResponseType(typeof(Notification))]
+        public IHttpActionResult PostNotification(Notification notification)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Notifications.Add(notification);
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateException)
+            {
+                if (NotificationExists(notification.ID))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtRoute("DefaultApi", new { id = notification.ID }, notification);
+        }
+
+        // DELETE: api/NotificationAPIs/5
+        [ResponseType(typeof(Notification))]
+        public IHttpActionResult DeleteNotification(int id)
+        {
+            Notification notification = db.Notifications.Find(id);
+            if (notification == null)
+            {
+                return NotFound();
+            }
+
+            db.Notifications.Remove(notification);
+            db.SaveChanges();
+
+            return Ok(notification);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool NotificationExists(int id)
+        {
+            return db.Notifications.Count(e => e.ID == id) > 0;
+        }
+    }
+}
