ee3f365c4bd2eb4abaf4eb90ff33c7c20bea4263
Gommash <a.lekedal@gmail.com>
2017-08-23 09:20:27 +0200

    Fixes

--- LMS-Lexicon/WebServer/Repository/DocumentRepository.cs
+++ LMS-Lexicon/WebServer/Repository/DocumentRepository.cs
@@ -1,65 +1,144 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Web;
-//using WebServer.Models;
-//using WebServer.Models.LMS;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebServer.Models;
+using WebServer.Models.LMS;
 
-//namespace WebServer.Repository
-//{
-//    public class DocumentRepository
-//    {
+namespace WebServer.Repository
+{
+    public class DocumentRepository
+    {
 
-//        public DocumentRepository docRepo = new DocumentRepository();
+        private ApplicationDbContext db = new ApplicationDbContext();
 
-//        public Document document { get; set; }
 
-//        public IEnumerable<Document> Documents()
-//        {
-//            return docRepo.Document;
-//        }
 
-//        //Add
-//        public void Add(Document document)
-//        {
-//            docRepo.Documents.Add(document);
-//            docRepo.SaveChanges();
-//        }
+        public Document document { get; set; }
 
-//        public Document Document(int? id)
-//        {
-//            return Documents().FirstOrDefault(d => d.ID == id);
-//        }
 
-//        //Delete
-//        public void Delete(int id)
-//        {
-//            Document document = Document(id);
-//            if (document != null)
-//            {
-//                docRepo.Documents.Remove(document);
-//                docRepo.SaveChanges();
-//            }
-//        }
-//        private void SaveChanges()
-//        {
-//            docRepo.SaveChanges();
-//        }
-//        #region IDisposable
-//        private bool disposedValue = false;
 
-//        protected virtual void Dispose(bool disposing)
-//        {
-//            if (!disposedValue)
-//            {
-//                docRepo.Dispose();
-//                disposedValue = true;
-//            }
-//        }
-//        public void Dispose()
-//        {
-//            Dispose(true);
-//        }
-//        #endregion
-//    }
-//}
+        public IEnumerable<Document> Documents()
+        {
+
+            return db.Documents;
+
+        }
+
+
+
+        /// <summary>
+
+        /// Returns the list of documents uploaded for a given course
+
+        /// </summary>
+
+        /// <param name="courseId">Course ID</param>
+
+        /// <returns></returns>
+
+        public IEnumerable<Document> Documents(int courseId)
+        {
+
+            return db.Documents.Where(d => d.CourseID == courseId);
+
+        }
+
+
+
+        /// <summary>
+
+        /// Returns the list of documents uploaded by a user
+
+        /// </summary>
+
+        /// <param name="userId">User's ID</param>
+
+        /// <returns></returns>
+
+        public IEnumerable<Document> Documents(string userId)
+        {
+
+            return db.Documents.Where(d => d.UploaderID == userId);
+
+        }
+
+
+
+        //Add
+
+        public void Add(Document document)
+        {
+
+            db.Documents.Add(document);
+
+            db.SaveChanges();
+
+        }
+
+
+
+        public Document Document(int? id)
+        {
+
+            return Documents().FirstOrDefault(d => d.ID == id);
+
+        }
+
+
+
+        //Delete
+
+        public void Delete(int id)
+        {
+
+            Document document = Document(id);
+
+            if (document != null)
+            {
+
+                db.Documents.Remove(document);
+
+                db.SaveChanges();
+
+            }
+
+        }
+
+        private void SaveChanges()
+        {
+
+            db.SaveChanges();
+
+        }
+
+        #region IDisposable
+
+        private bool disposedValue = false;
+
+
+
+        protected virtual void Dispose(bool disposing)
+        {
+
+            if (!disposedValue)
+            {
+
+                db.Dispose();
+
+                disposedValue = true;
+
+            }
+
+        }
+
+        public void Dispose()
+        {
+
+            Dispose(true);
+
+        }
+
+        #endregion
+    }
+}
