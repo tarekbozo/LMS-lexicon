db8266b3398988d04ddf68f97739a6fbff6a21fb
AhAh10 <ahmadi2001@yahoo.com>
2017-08-29 09:49:06 +0200

    Hopfully a good fix

---
+++ LMS-Lexicon/WebClient_Lexicon/Scripts/Angular/Attendance/Attendance.js
@@ -0,0 +1,13 @@
+﻿(function () {
+    var app = angular.module("LMSApp");
+    app.controller("AttendancesController", ["$scope", "$http", function ($scope, $http) {
+        var serverBaseUrl = "http://localhost:51942";
+        $scope.getData = function () {
+            $http.get('http://localhost:51942/api/ Attendance/Get/')
+                .then(function (response) {
+                    $scope.data = response.data;
+                });
+        };
+
+    }]);
+}());
---
+++ LMS-Lexicon/WebClient_Lexicon/Scripts/Angular/Document/Document.js
@@ -0,0 +1,13 @@
+﻿(function () {
+    var app = angular.module("LMSApp");
+    app.controller("DocumentAPIController", ["$scope", "$http", function ($scope, $http) {
+        var serverBaseUrl = "http://localhost:51942";
+        $scope.getData = function () {
+            $http.get('http://localhost:51942/api/Document/Get/')
+                .then(function (response) {
+                    $scope.data = response.data;
+                });
+        };
+      
+    }]);
+}());
