c8065bdc96ff84ab00c563aadf5534c877d0003f
AhAh10 <ahmadi2001@yahoo.com>
2017-08-29 11:02:51 +0200

    Added JS files and other stuff

--- LMS-Lexicon/WebClient_Lexicon/Scripts/Angular/Course/Course.js
+++ LMS-Lexicon/WebClient_Lexicon/Scripts/Angular/Course/Course.js
@@ -1,9 +1,9 @@
 ﻿(function () {
     var app = angular.module("LMSApp");
-    app.controller("AttendancesController", ["$scope", "$http", function ($scope, $http) {
+    app.controller("CoursesController", ["$scope", "$http", function ($scope, $http) {
         var serverBaseUrl = "http://localhost:51942";
         $scope.getData = function () {
-            $http.get('http://localhost:51942/api/Attendance/Get/')
+            $http.get('http://localhost:51942/api/Course/Get/')
                 .then(function (response) {
                     $scope.data = response.data;
                 });
